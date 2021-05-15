fc803e7a63a960385f6396002696b2e30f1453c0
Nicolai <72386426+Nicolai-boiko@users.noreply.github.com>
2021-05-15 12:06:42 +0300

    feat: reviews slider

--- src/modules/reviewsSlider.js
+++ src/modules/reviewsSlider.js
@@ -1,14 +1,31 @@
-import SliderCarusel from './slider';
-
 const reviewsSlider = () => {
-    const reviewSlider = new SliderCarusel({
-        main: '.reviews-slider-wrap',
-        wrap: '.reviews-slider',
-        next: '#reviews-arrow_right',
-        prev: '#reviews-arrow_left',
-        slidesToShow: 1,
-        infinity: true,
-    });
-    reviewSlider.init();
+    const reviewsSlides = document.querySelectorAll('.reviews-slider__slide');
+    const nextFrame = document.querySelector('#reviews-arrow_right');
+    const prevFrame = document.querySelector('#reviews-arrow_left');
+    nextFrame.style.transform = 'rotate(90deg)';
+    prevFrame.style.transform = 'rotate(90deg)';
+
+
+    let framePos = 0;
+    const moveSlider = (framePos) => {
+        reviewsSlides.forEach(slide => {
+            slide.style.transform = `translateY(${-100 * framePos}%)`;
+            slide.style.transition = `transform 0.5s`;
+        })
+    }
+    nextFrame.addEventListener('click', () => {
+        ++framePos
+        if (framePos > reviewsSlides.length - 1) {
+            framePos = 0
+        }
+        moveSlider(framePos);
+    })
+    prevFrame.addEventListener('click', () => {
+        --framePos
+        if (framePos < 0) {
+            framePos = reviewsSlides.length - 1;
+        }
+        moveSlider(framePos);
+    })
 }
 export default reviewsSlider;
